b754e98f9553563a9d26ebf4d792333b40569be0
Afroz khan <ahmadafroz535@gmail.com>
2022-09-26 06:48:34 +0530

    deve:find fequency in a given string

---
+++ code.java
@@ -0,0 +1,27 @@
+package com.abc.gitadd;
+
+public class FrequencyFind {
+
+	public static void main(String[] args) {
+		String s="COMPUTERSCIENCE";
+		char [] ch=s.toCharArray();
+		int [] ar = new int [26];
+		for(int i=0;i<ch.length;i++)
+		{
+		int index	=(int)ch[i]-65;
+		ar[index]=ar[index]+1;
+		}
+		int count=0;
+		for(int j=0;j<ar.length;j++)
+		{
+			if(ar[j]>1)
+			{
+				System.out.println((char)(j+65)+"Repeated");
+			}
+			
+		}
+		
+		
+	}
+
+}
