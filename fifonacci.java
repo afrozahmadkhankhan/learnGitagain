c6bc1a17e4b36774b187a165216158c6ca6ef741
Afroz khan <ahmadafroz535@gmail.com>
2022-09-28 06:42:01 +0530

    afroz/TB-10:find the fifonacci seriese in java

---
+++ fifonacci.java
@@ -0,0 +1,25 @@
+package com.abc.gitadd;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.TreeSet;
+
+public class FrequencyFind {
+
+	public static void main(String[] args) {
+		
+		int  a=0;
+		int b=1;
+		int c;
+		System.out.println(a+" "+b);
+		for(int i=0;i<10;i++)
+		{
+			 c=a+b;
+			 System.out.println(c);
+			 a=b;
+			 b=c;
+		}
+		
+	}
+}
