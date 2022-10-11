e7caaa88ab36ec94d2ca959e76983b0e55eb949e
Afroz khan <ahmadafroz535@gmail.com>
2022-10-11 07:18:42 +0530

    afroz/TB-23:This is use for gcd

--- lcmGcd.java
+++ lcmGcd.java
@@ -1,4 +1,3 @@
-package com.abc.gitadd;
 
 import java.util.Scanner;
 
@@ -8,20 +7,23 @@ public class Alphanumeric {
 		Scanner scan = new Scanner(System.in);
 		int n1 = scan.nextInt();
 		int n2 = scan.nextInt();
-		solve(n1, n2);
+		int temp=solve(n1, n2);
+		System.out.println(temp);
 
 	}
 
-	public static void solve(int n1, int n2) {
-		int max = 0;
-		max = (n1 > n2) ? n1 : n2;
-		while (true) {
-			if (max % n1 == 0 && max % n2 == 0) {
-				System.out.println(max);
-				System.exit(0);
+	public static int solve(int n1, int n2) {
+		int min = 0;
+		int gcd=0;
+		min = (n1 < n2) ? n1 : n2;
+		for (int i=1;i<=min;i++) {
+			if (n1%i == 0 && n2 % i == 0) {
+				
+				gcd=i;
 			}
-			max++;
+		
 		}
+		return gcd;
 	}
 
 }
