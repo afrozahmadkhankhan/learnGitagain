17e588b39fa4e2a73e61e0254d5d3a3a7c1bf729
Afroz khan <ahmadafroz535@gmail.com>
2022-12-22 07:27:40 +0530

    afroz/TB-58:finding the factorial

---
+++ factorial.java
@@ -0,0 +1,18 @@
+package com.abc.gitadd;
+
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class SortingName {
+
+	public static void main(String[] args) {
+		int s = 5;
+		int fact = 1;
+		for (int i = 1; i <= s; i++) {
+			fact = fact * i;
+		}
+		System.out.println(fact);
+
+	}
+}
